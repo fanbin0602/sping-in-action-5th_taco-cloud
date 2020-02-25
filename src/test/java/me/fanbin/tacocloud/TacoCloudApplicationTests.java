6d7aeb2f984c3c39e4ba8220c2b38066ae76054c
Fan Bin <fanbin0602@163.com>
2020-02-25 18:38:36 +0800

    chapter 1

---
+++ src/test/java/me/fanbin/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fanbin.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
